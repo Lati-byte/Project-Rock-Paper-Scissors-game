84ea47ba3eff5756a31b59968515a0d854fb5406
Lati-byte <harmony.rams@gmail.com>
2023-12-11 14:11:50 +0200

    added a function named game( )
    the function uses the previous functions inside of it to play a best-of-five game that keeps score and reports a winner or loser at the end.

--- app.js
+++ app.js
@@ -23,3 +23,32 @@ function getComputerChoice() {
       return 'It\'s a Tie! Play again.';
     }
   }
+
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let i = 0; i < 5; i++) {
+      const playerSelection = prompt('Enter your choice: Rock, Paper, or Scissors');
+      const computerSelection = getComputerChoice();
+      const roundResult = playRound(playerSelection, computerSelection);
+  
+      console.log(roundResult);
+  
+      if (roundResult.includes('Win')) {
+        playerScore++;
+      } else if (roundResult.includes('Lose')) {
+        computerScore++;
+      }
+    }
+  
+    if (playerScore > computerScore) {
+      console.log('Congratulations! You win the game!');
+    } else if (playerScore < computerScore) {
+      console.log('Sorry, you lose the game. Better luck next time!');
+    } else {
+      console.log('It\'s a tie game!');
+    }
+  }
+
+  game();
