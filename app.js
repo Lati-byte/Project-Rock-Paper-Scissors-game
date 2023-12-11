1973ee5ae964061a7ff056f38cdc50469a18c739
Lati-byte <harmony.rams@gmail.com>
2023-12-11 12:37:34 +0200

    Added a function named playRound
    playRound( ) is a function that takes in two parameters. The function returns the result of the round as a string which can be logged or used in future processing.

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }
+
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+  
+    if (
+      (playerSelection === 'rock' && computerSelection === 'Scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'Rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'Paper')
+    ) {
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (
+      (playerSelection === 'scissors' && computerSelection === 'Rock') ||
+      (playerSelection === 'rock' && computerSelection === 'Paper') ||
+      (playerSelection === 'paper' && computerSelection === 'Scissors')
+    ) {
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return 'It\'s a Tie! Play again.';
+    }
+  }
